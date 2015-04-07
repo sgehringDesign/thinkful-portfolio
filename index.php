364e81fca9e4145e0e0634a62cf13bd26de69629
Stephen Gehring <sgehringdesign@gmail.com>
2015-04-07 03:08:55 -0700

    hashing out really rough draft html and css

--- index.php
+++ index.php
@@ -7,14 +7,30 @@
     <?php
         include $_SERVER['DOCUMENT_ROOT']."/includes/head.php";
     ?>
+    <style>
+    #hero-inner {
+        background: url('/i/placeholders/IMG_4880.jpg') no-repeat 50% 95%;  /*lifebuzz-c92e3fc6a2d898579ece3249a188e1a7-limit_2000.jpg  50% 60%*/  
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+        min-height:500px;
+    }
+    </style>
 </head>
 <body>
     <?php
         include $_SERVER['DOCUMENT_ROOT']."/includes/nav.topbar.php";
     ?>
     <section id="hero" class="row">
-        <div class="large-12 columns">
-            <img src="s.gif" style="background:#ccc;" border="0" />
+        <div id="hero-inner" style="">
+            <div class="row">
+                <div class="hide small-12 large-offset-4 large-5 columns">
+                    <h1 style="color:#fff; line-height:1em;">Syndicate Customized Applications</h1>
+                    <p style="color:#fff;" class="lead"></p>
+                </div>
+            </div>
+            <!--img src="g" class="" style="max-width: 100%; height: auto;" width="100%" border="0" -->
         </div>
     </section>
     
