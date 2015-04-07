b7e66d7cd0e6a0a4d59a266159544022474237f6
Stephen Gehring <sgehringdesign@gmail.com>
2015-04-07 00:37:37 -0700

    adding includes

---
+++ includes/head.php
@@ -0,0 +1,2 @@
+    <link rel="stylesheet" href="css/app.css" type="text/css">
+    <script src="/js/frameworks.min.js" type="text/javascript"></script>
---
+++ includes/nav.topbar.php
@@ -0,0 +1,17 @@
+    <nav class="top-bar" data-topbar role="navigation">
+        <ul class="title-area">
+            <li class="name">
+                <h1><a href="#">S.Gehring Design</a></h1>
+            </li>
+            <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+        </ul>
+
+        <section class="top-bar-section">
+            <ul class="right">
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Blog</a></li>
+            </ul>
+        </section>
+    </nav>
